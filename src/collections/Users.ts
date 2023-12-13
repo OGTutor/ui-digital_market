7eb3e6c9b8ef5dc7bb3e04872b6764fdc21127ae
OGTutor <i.send.you.a.letter@gmail.com>
2023-12-13 19:39:52 +0200

    Some fixes

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,14 @@ import { CollectionConfig } from 'payload/types';
 
 export const Users: CollectionConfig = {
 	slug: 'users',
-	auth: true,
+	auth: {
+		verify: {
+			generateEmailHTML: ({ token }) => {
+				return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}
+				/verify-email?token=${token}'>Verify account</a>`;
+			},
+		},
+	},
 	access: {
 		read: () => true,
 		create: () => true,
@@ -10,6 +17,8 @@ export const Users: CollectionConfig = {
 	fields: [
 		{
 			name: 'role',
+			defaultValue: 'user',
+			required: true,
 			type: 'select',
 			options: [
 				{ label: 'Admin', value: 'admin' },
