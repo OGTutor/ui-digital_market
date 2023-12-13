647a5f18c90ff5976a1f8251790ff6137b4798d4
OGTutor <i.send.you.a.letter@gmail.com>
2023-12-13 19:37:04 +0200

    Implemented TRPC & Payload libraries`s setup for backend

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
+import { inferAsyncReturnType } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
 import { getPayloadClient } from './get-payload';
 import { nextApp, nextHandler } from './next-utils';
+import { appRouter } from './trpc';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+const createContext = ({
+	req,
+	res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+	req,
+	res,
+});
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
 const start = async () => {
 	const payload = await getPayloadClient({
@@ -15,15 +28,23 @@ const start = async () => {
 		},
 	});
 
+	app.use(
+		'/api/trpc',
+		trpcExpress.createExpressMiddleware({
+			router: appRouter,
+			createContext,
+		})
+	);
+
 	app.use((req, res) => nextHandler(req, res));
 
 	nextApp.prepare().then(() => {
-		// payload.logger.info('Next.js started!');
+		payload.logger.info('Next.js started!');
 
 		app.listen(PORT, async () => {
-			// payload.logger.info(
-			// 	`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-			// );
+			payload.logger.info(
+				`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+			);
 		});
 	});
 };
