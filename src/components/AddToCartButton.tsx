60f1c8e5f3d1542e013304b3c160ea1fe9cd211f
OGTutor <i.send.you.a.letter@gmail.com>
2024-01-18 17:40:33 +0200

    Implemented an AddToCartButton component

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { FC, useEffect, useState } from 'react';
+import { Button } from './ui/button';
+
+const AddToCartButton: FC = () => {
+	const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setIsSuccess(false);
+		}, 2000);
+
+		return () => clearTimeout(timeout);
+	}, [isSuccess]);
+
+	return (
+		<Button
+			onClick={() => {
+				setIsSuccess(true);
+			}}
+			size="lg"
+			className="w-full"
+		>
+			{isSuccess ? 'Added!' : 'Add to cart'}
+		</Button>
+	);
+};
+
+export default AddToCartButton;
